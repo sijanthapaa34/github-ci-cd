a6a7e9eb2f0190ef203c47f7000619c47f92d881
sijanthapaa34 <sijanmgr034@gmail.com>
2025-05-29 17:37:45 +0545

    first commit

---
+++ src/main/java/com/sijan/spring_boot_docker/SpringBootDockerApplication.java
@@ -0,0 +1,20 @@
+package com.sijan.spring_boot_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+	@GetMapping("/getmessage")
+	public String getMessage() {
+		return "waasup";
+	}
+}
---
+++ src/test/java/com/sijan/spring_boot_docker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sijan.spring_boot_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
